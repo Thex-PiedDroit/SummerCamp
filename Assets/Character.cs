4ce81d103d3c7faa1049494899e744b717982af6
Thex <thex-pieddroit@hotmail.com>
2018-07-10 16:38:06 +0200

    Basic character 3D control

---
+++ Assets/Character.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+#region Variables (public)
+
+	public Rigidbody m_pRigidBody = null;
+
+	public float m_fSpeed = 0.0f;
+
+	#endregion
+
+#region Variables (private)
+
+
+
+	#endregion
+
+
+	private void Update()
+	{
+		Vector3 tMovement = new Vector3
+		{
+			x = Input.GetAxis("Horizontal"),
+			z = Input.GetAxis("Vertical")
+		};
+
+		if (tMovement != Vector3.zero)
+		{
+			m_pRigidBody.MovePosition(m_pRigidBody.position + tMovement * (m_fSpeed * Time.deltaTime));
+			transform.forward = tMovement.normalized;
+		}
+	}
+}
