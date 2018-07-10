814957c0277830aaa3df33e6881e8323dd0819dd
Thex <thex-pieddroit@hotmail.com>
2018-07-10 18:03:55 +0200

    Basic camera follow behaviour

---
+++ Assets/PlayerCamera.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+#region Variables (public)
+
+	public Transform m_pTarget = null;
+
+	public float m_fFollowDistance = 0.0f;
+
+	#endregion
+
+#region Variables (private)
+
+	private Vector3 m_tLastTargetPosition = Vector3.zero;
+
+	#endregion
+
+
+	private void LateUpdate()
+	{
+		if (m_pTarget != null && m_pTarget.position != m_tLastTargetPosition)
+			FollowTarget();
+	}
+
+	private void FollowTarget()
+	{
+		transform.position = m_pTarget.position - (transform.forward * m_fFollowDistance);
+		m_tLastTargetPosition = m_pTarget.position;
+	}
+}
