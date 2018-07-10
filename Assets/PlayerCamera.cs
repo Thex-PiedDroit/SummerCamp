de7ca06aef308f8cb7059defecffe349ceefdef3
Thex <thex-pieddroit@hotmail.com>
2018-07-10 18:22:10 +0200

    Camera rotation around target based on mouse input

--- Assets/PlayerCamera.cs
+++ Assets/PlayerCamera.cs
@@ -9,6 +9,9 @@ public class PlayerCamera : MonoBehaviour
 
 	public float m_fFollowDistance = 0.0f;
 
+	[Tooltip("In degrees per second")]
+	public float m_fYRotationSpeed = 0.0f;
+
 	#endregion
 
 #region Variables (private)
@@ -18,15 +21,30 @@ public class PlayerCamera : MonoBehaviour
 	#endregion
 
 
+	private void Start()
+	{
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
 	private void LateUpdate()
 	{
+		CatchCameraRotationInput();
+
 		if (m_pTarget != null && m_pTarget.position != m_tLastTargetPosition)
 			FollowTarget();
 	}
 
+	private void CatchCameraRotationInput()
+	{
+		float fYRotation = Input.GetAxis("Mouse X");
+		if (fYRotation != 0.0f)
+			transform.localEulerAngles += new Vector3(0.0f, fYRotation * m_fYRotationSpeed, 0.0f) * Time.deltaTime;
+	}
+
 	private void FollowTarget()
 	{
-		transform.position = m_pTarget.position - (transform.forward * m_fFollowDistance);
+		transform.position = m_pTarget.position;
 		m_tLastTargetPosition = m_pTarget.position;
 	}
 }
