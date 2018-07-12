94abe94cd28a4281fd227eccf378eeed463bd373
Thex <thex-pieddroit@hotmail.com>
2018-07-12 16:11:08 +0200

    Basic Enemy prefab with no movement

---
+++ Assets/Scripts/EnemyCharacter.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyCharacter : Character
+{
+#region Variables (public)
+
+	public NavMeshAgent m_pNavMeshAgent = null;
+
+	#endregion
+
+#region Variables (private)
+
+
+
+	#endregion
+
+
+	private void Start()
+	{
+		m_pNavMeshAgent.speed = m_fSpeed;
+	}
+}
