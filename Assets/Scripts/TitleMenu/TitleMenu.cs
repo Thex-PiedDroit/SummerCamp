f003595b829a2018c8a899c56aad5b066daaebee
Thex <thex-pieddroit@hotmail.com>
2018-07-12 13:59:34 +0200

    Functionality to Play and Quit buttons in TitleMenu

---
+++ Assets/Scripts/TitleMenu/TitleMenu.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleMenu : MonoBehaviour
+{
+#region Variables (public)
+
+
+
+	#endregion
+
+#region Variables (private)
+
+
+
+	#endregion
+
+
+	public void LaunchGame(string sSceneName)
+	{
+		SceneManager.LoadScene(sSceneName, LoadSceneMode.Single);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
